19064232d79ab277e1248fb4e5c51681733351ab
wlals <jiminelp@gmail.com>
2023-09-15 16:40:03 +0900

    ✨💄🎨 :: 라우팅, 파일구조 개선, 글로벌 스타일 추가

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-const Header = () => {
-  return <></>;
+const Header = ({ page = 0, user }) => {
+  return (
+    <Container>
+      {/* <HeaderLogo>대팔이</HeaderLogo>
+      <FlexContainer>
+        <SearchBar>
+          <InputBar />
+          <SearchIcon />
+        </SearchBar>
+        <UserImg />
+        <WelcomeText>{userName} 님 안녕하세요</WelcomeText>
+      </FlexContainer> */}
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  width: 100vw;
+  height: 120px;
+  /* background-color: */
+`;
 
 export default Header;
---
+++ src/style/GLBStyle.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
+      @font-face {
+      font-family: Pretendard;
+      font-weight: 400;
+      src: url(@assets/fonts/Pretendard-Regular.woff) format('font-woff');
+    }
+  }
+  body {
+    width: 100vw;
+    height: 100vh;
+    overflow:hidden;
+    margin: 0;
+    background-color: #181738;
+  }
+  input {
+    border: 0;
+    outline: 0;
+  }
+`;
+
+export default GlobalStyle;
