a82610b063f4d70e744cc0bbc8af33f83be49534
wlals <jiminelp@gmail.com>
2023-09-17 22:30:56 +0900

    💄:: 메인페이지 스타일링, 애니메이션 추가

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,34 +1,119 @@
 import React from "react";
-import styled from "styled-components";
-import Product from "../components/product";
+import styled, { keyframes } from "styled-components";
+import ProductPost from "../components/product";
+import Header from "../components/header";
 
 const MainPage = () => {
   return (
     <Container>
+      <Header page={0} />
       <StylingLobby>
         <SmallText>대마고 전용 중고거래 사이트</SmallText>
         <Logo>대팔이</Logo>
       </StylingLobby>
       <GrowContainer>
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
+        <ProductPost />
       </GrowContainer>
     </Container>
   );
 };
 
-const Container = styled.main``;
-const StylingLobby = styled.div``;
-const SmallText = styled.span``;
-const Logo = styled.span``;
-const GrowContainer = styled.div``;
+const fadeIn = keyframes`
+    0% {
+        transform: translateX(-64px);
+        opacity: 0;
+    }
+    100% {
+        transform: translateX(0);
+        opacity: 1;
+    }
+`;
+
+const Container = styled.main`
+  width: 100vw;
+`;
+
+const StylingLobby = styled.div`
+  height: 830px;
+  background-image: url("./wave.svg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position-y: bottom;
+  position: relative;
+  display: flex;
+`;
+
+const SmallText = styled.span`
+  color: white;
+  text-align: center;
+  font-size: 64px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  animation: ${fadeIn} 2s;
+  position: absolute;
+  top: 308px;
+  left: 284px;
+  font-family: Hakgyoansim Wooju;
+`;
+
+const Logo = styled.span`
+  color: #fff;
+  text-align: center;
+  font-size: 128px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  animation: ${fadeIn} 2s;
+  position: absolute;
+  top: 276px;
+  right: 330px;
+  font-family: Hakgyoansim Wooju;
+`;
+
+const GrowContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  column-gap: auto;
+  row-gap: 90px; /* 세로 간격을 90px로 설정 */
+  margin-left: 128px; /* 좌측 여백 128px */
+  margin-right: 128px; /* 우측 여백 128px */
+  justify-items: center;
+  position: relative;
+  top: -230px;
+`;
 
 export default MainPage;
