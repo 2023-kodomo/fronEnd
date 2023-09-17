1148b4b12d432ed6da2474aa2a8dc2062d060c44
wlals <jiminelp@gmail.com>
2023-09-17 22:32:51 +0900

    💄:: 제품 썸네일 컴포넌트 스타일링

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -1,8 +1,67 @@
 import React from "react";
 import styled from "styled-components";
 
-const Product = () => {
-  return <></>;
+const ProductPost = ({ Product }) => {
+  let name = "코로나 팔아요";
+  let productImg;
+  let price = "₩ 19,000";
+  if (Product !== undefined) {
+    name = Product.title;
+    productImg = Product.image;
+    price = Product.price;
+  }
+  return (
+    <Container>
+      <ProductImg src="./img/undefined.svg" />
+      <ProductName>{name}</ProductName>
+      <ProductPrice>{price}</ProductPrice>
+    </Container>
+  );
 };
 
-export default Product;
+const Container = styled.summary`
+  background-color: #181738;
+  box-sizing: border-box;
+  border: 5px solid #7a6ccf;
+  border-radius: 48px 48px 0 0;
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  height: 300px;
+  padding-top: 60px;
+  align-items: center;
+  gap: 15px;
+  flex-shrink: 0;
+`;
+
+const ProductImg = styled.img`
+  width: 140px;
+  height: 140px;
+  background-color: #c2c2c2;
+`;
+
+const ProductName = styled.span`
+  color: #fff;
+  text-align: center;
+
+  /* 20pt 학교안심 우주 */
+  font-family: "Hakgyoansim Wooju";
+  font-size: 20px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const ProductPrice = styled.span`
+  color: #fff;
+  text-align: center;
+
+  /* 24pt 학교안심 우주 */
+  font-family: "Hakgyoansim Wooju";
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export default ProductPost;
