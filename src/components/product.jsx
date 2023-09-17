ab685b05d0e813b02fd824c0c354ccc204f2a4f4
wlals <jiminelp@gmail.com>
2023-09-17 23:09:30 +0900

    💄:: 제품 썸네일 그림자 추가

--- src/components/product.jsx
+++ src/components/product.jsx
@@ -32,6 +32,7 @@ const Container = styled.summary`
   align-items: center;
   gap: 15px;
   flex-shrink: 0;
+  box-shadow: 0px 0px 16px 4px rgba(0, 0, 0, 0.25);
 `;
 
 const ProductImg = styled.img`
