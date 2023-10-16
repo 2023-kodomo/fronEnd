669478f3b174330da824ccfd95099e599fd31737
wlals <jiminelp@gmail.com>
2023-10-16 22:42:55 +0900

    ✨📝💄:: 댓글 추가 기능 구현

--- src/components/productinfopage/ProductinfoComponent.jsx
+++ src/components/productinfopage/ProductinfoComponent.jsx
@@ -1,20 +1,123 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState, useMemo } from "react";
 import styled, { keyframes } from "styled-components";
 import Header from "../HeaderComponent";
 import StylingLobby from "../stylingLobby";
 import Comment from "./comment";
 
 const ProductInfoComponent = () => {
+  const commentInput = useRef();
+  const contentBox = useRef();
+  const [comments, setComments] = useState();
+
+  useEffect(() => {
+    const datas = {
+      id: null,
+      seller: {
+        id: "650bbb50c65a705c09394da6",
+        email: "test@email.com",
+        name: "테스ㅈㅈ트",
+        profileImage: null,
+        role: "GUEST",
+      },
+      title: "테스트 상품",
+      content: "테스트",
+      price: 0,
+      image: "테스트",
+      uploadDate: "2023-09-22T05:52:54.581+00:00",
+      comment: [
+        {
+          writer: {
+            id: "650bbb50c65a705c09394da6",
+            email: "test@email.com",
+            name: "테스ㅈㅈ트",
+            profileImage: null,
+            role: "GUEST",
+          },
+          content: "하하!",
+          createdDate: "2023-09-22T06:15:47.878+00:00",
+        },
+      ],
+      place: "미정",
+    };
+
+    setComments(() => {
+      const { comment } = datas;
+      return comment;
+    });
+  }, []);
+
+  const addComment = () => {
+    setComments([
+      ...comments,
+      {
+        writer: {
+          id: "650bbb50c65a705c09394da6",
+          email: "test@email.com",
+          name: "테스트입니다",
+          profileImage: null,
+          role: "GUEST",
+        },
+        content: `${commentInput.current.value}`,
+        createdDate: getCurrentTimeFormatted(),
+      },
+    ]);
+  };
+
+  const getCurrentTimeFormatted = () => {
+    const currentDate = new window.Date();
+    const year = currentDate.getFullYear();
+    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+    const day = String(currentDate.getDate()).padStart(2, "0");
+    const hours = String(currentDate.getHours()).padStart(2, "0");
+    const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
+    const milliseconds = String(currentDate.getMilliseconds()).padStart(3, "0");
+    const timezoneOffset = currentDate.getTimezoneOffset();
+
+    const timezoneSign = timezoneOffset > 0 ? "-" : "+";
+
+    const timezoneHours = String(
+      Math.abs(Math.floor(timezoneOffset / 60))
+    ).padStart(2, "0");
+    const timezoneMinutes = String(Math.abs(timezoneOffset % 60)).padStart(
+      2,
+      "0"
+    );
+
+    const formattedTime = `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.${milliseconds}${timezoneSign}${timezoneHours}:${timezoneMinutes}`;
+
+    return formattedTime;
+  };
+
+  const makeCommentComponent = (data) => {
+    if (data) {
+      return data.map((e, i) => {
+        return (
+          <Comment
+            content={e.content}
+            user={e.writer}
+            createdDate={e.createdDate}
+            key={i}
+          />
+        );
+      });
+    }
+  };
+
+  useEffect(() => {
+    console.log(comments);
+  }, [comments]);
+
   return (
     <Container>
       <Header page={0} />
-      <ContentBox>
+      <StylingLobby></StylingLobby>
+      <ContentBox ref={contentBox}>
         <a href={"/"}>
           <BeforeButton>
             <img src="./img/Arrow1.svg" alt="" />
           </BeforeButton>
         </a>
-
         <TextGroup>
           <Title>일이삼사오육칠팔구십일이삼사오육칠팔구십</Title>
           <Date>2023.09.01</Date>
@@ -30,13 +133,15 @@ const ProductInfoComponent = () => {
         <AddCommentContainer>
           <div>
             <span>댓글 작성</span>
-            <WriteButton>작성</WriteButton>
+            <WriteButton onClick={addComment}>작성</WriteButton>
           </div>
-          <CommentTextArea placeholder="댓글 내용을 입력해주세요"></CommentTextArea>
+          <CommentTextArea
+            placeholder="댓글 내용을 입력해주세요"
+            ref={commentInput}
+          ></CommentTextArea>
         </AddCommentContainer>
-        <Comment />
+        {comments && makeCommentComponent(comments)}
       </ContentBox>
-      <StylingLobby></StylingLobby>
     </Container>
   );
 };
@@ -51,12 +156,16 @@ const rotate = keyframes`
 `;
 
 const Container = styled.div`
+  box-sizing: border-box;
   width: 100vw;
+  height: 100vh;
   position: relative;
 `;
 
 const ContentBox = styled.div`
-  z-index: 9;
+  max-height: 75vh;
+  min-width: 280px;
+  overflow-y: scroll;
   box-sizing: border-box;
   padding-top: 46px;
   width: 52%;
@@ -69,6 +178,16 @@ const ContentBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  &::-webkit-scrollbar {
+    width: 5px;
+    border-radius: 50%;
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: #7a6ccf; /*스크롤바의 색상*/
+  }
+  &::-webkit-scrollbar-track {
+    background-color: transparent; /*스크롤바 트랙 색상*/
+  }
 `;
 
 const BeforeButton = styled.button`
--- src/components/productinfopage/comment.jsx
+++ src/components/productinfopage/comment.jsx
@@ -1,10 +1,60 @@
 import React from "react";
 import styled from "styled-components";
 
-const Comment = () => {
-  return <Container></Container>;
+const Comment = (props) => {
+  console.log(props);
+  return (
+    <Container>
+      <FlexContainer>
+        <UserName>{props.user.name}</UserName>
+        <CreateDate>{props.createdDate}</CreateDate>
+      </FlexContainer>
+      <hr />
+      <Content>{props.content}</Content>
+    </Container>
+  );
 };
 
-const Container = styled.div``;
+const Container = styled.div`
+  border-radius: 15px;
+  width: 87%;
+  background: #d0d0d0;
+  position: relative;
+  margin-top: 43px;
+  hr {
+    background: #181738;
+    height: 1px;
+    border: 0;
+    margin: 0;
+  }
+`;
+
+const FlexContainer = styled.div`
+  display: flex;
+  padding: 1% 2%;
+  justify-content: space-between;
+  flex-wrap: wrap;
+`;
+
+const UserName = styled.span`
+  color: #000;
+  font-family: Hakgyoansim Wooju;
+  font-size: 16px;
+`;
+
+const CreateDate = styled.span`
+  color: #000;
+  font-family: Hakgyoansim Wooju;
+  font-size: 16px;
+`;
+
+const Content = styled.p`
+  color: #000;
+  font-family: Hakgyoansim Wooju;
+  font-size: 20px;
+  margin: 0px;
+  padding: 8px 1em;
+  white-space: pre-wrap;
+`;
 
 export default Comment;
