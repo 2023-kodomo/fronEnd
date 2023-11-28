684a42526a3c88503fb7cb365f044ad30efc020b
wlals <jiminelp@gmail.com>
2023-11-28 19:40:07 +0900

    ✨:: 토큰 연장 함수 구현

---
+++ src/utils/api/reissue.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
+const baseURL = process.env.REACT_APP_BASE_URL;
+
+const reissue = async (navigate) => {
+  return await axios
+    .post(`${baseURL}/auth/reissue`, {
+      refreshToken: localStorage.getItem("refreshToken"),
+    })
+    .then((res) => {
+      localStorage.setItem("accessToken", res.data.accessToken);
+      localStorage.setItem("refreshToken", res.data.refreshToken);
+      window.location.reload();
+    })
+    .catch((rej) => {
+      console.log(rej.response.data);
+      if (
+        rej.response.data.status === 400 &&
+        rej.response.data.message === "잘못 된 데이터"
+      ) {
+        alert("토큰이 만료되었습니다. 다시 로그인 해주세요!");
+        navigate("/");
+      }
+      return rej;
+    });
+};
+
+export default reissue;
