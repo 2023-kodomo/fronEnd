9accf9d131ba151ce0240aa536bef8a1d8f37406
wlals <jiminelp@gmail.com>
2023-12-01 20:03:12 +0900

    📝💄:: 이메일 인증 페이지 퍼블리싱

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -18,34 +18,36 @@ const Header = ({ page = 0, user }) => {
       <Linker href="/">
         <HeaderLogo>대팔이</HeaderLogo>
       </Linker>
-      <FlexContainer>
-        <SearchBar>
-          <InputBar type="text" placeholder="검색할 상품을 입력해 주세요." />
-          <SearchIcon src="./img/Searchicon.svg" alt="SearchIcon" />
-        </SearchBar>
-        <UserImg src={user ? userImg : null} alt={user ? "UserImg" : ""} />
-        <WelcomeText>
-          <Linker href="/mypage">
-            <span className="Username">{userName}</span>
-          </Linker>
-          {user ? (
+      {page === 0 && (
+        <FlexContainer>
+          <SearchBar>
+            <InputBar type="text" placeholder="검색할 상품을 입력해 주세요." />
+            <SearchIcon src="./img/Searchicon.svg" alt="SearchIcon" />
+          </SearchBar>
+          <UserImg src={user ? userImg : null} alt={user ? "UserImg" : ""} />
+          <WelcomeText>
             <Linker href="/mypage">
-              <span className="username"> 님 안녕하세요</span>
+              <span className="Username">{userName}</span>
             </Linker>
-          ) : (
-            <>
-              <span className="LoginText">
-                로그인
-                <link href="#" />
-              </span>
-              <span className="SignUpText">
-                회원가입
-                <link href="#" />
-              </span>
-            </>
-          )}
-        </WelcomeText>
-      </FlexContainer>
+            {user ? (
+              <Linker href="/mypage">
+                <span className="username"> 님 안녕하세요</span>
+              </Linker>
+            ) : (
+              <>
+                <span className="LoginText">
+                  로그인
+                  <link href="#" />
+                </span>
+                <span className="SignUpText">
+                  회원가입
+                  <link href="#" />
+                </span>
+              </>
+            )}
+          </WelcomeText>
+        </FlexContainer>
+      )}
     </Container>
   );
 };
---
+++ src/pages/EmailPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import EmailPageComponent from "../components/emailpage/emailPageComponent";
+
+const EmailPage = () => {
+  return <EmailPageComponent email={"jiminelp@dsm.hs.kr"} />;
+};
+
+export default EmailPage;
