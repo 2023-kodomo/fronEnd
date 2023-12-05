5879c678d73be854bfacd833509b1dc4b71923a6
wlals <jiminelp@gmail.com>
2023-12-05 20:25:01 +0900

    🔥:: 더미값용 props 제거

--- src/pages/EmailPage.jsx
+++ src/pages/EmailPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import EmailPageComponent from "../components/emailpage/emailPageComponent";
 
 const EmailPage = () => {
-  return <EmailPageComponent email={"jiminelp@dsm.hs.kr"} />;
+  return <EmailPageComponent />;
 };
 
 export default EmailPage;
