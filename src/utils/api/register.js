e56d76dd59213124087cbb2540c90a1e2c771a5c
heedda <heedda.kollim@gmail.com>
2024-01-08 13:56:06 +0900

    회원가입 성공 시 문구 띄워주기

--- src/utils/api/register.js
+++ src/utils/api/register.js
@@ -9,16 +9,24 @@ const register = async (email, name, password) => {
       name: name,
       password: password,
     })
+    .then(() => {
+      alert("회원가입에 성공했습니다");
+    })
     .catch((rej) => {
+      console.log(rej.response);
       switch (rej.response.data["message"]) {
         case "이메일 형식이 올바르지 않습니다.":
           alert("학교 이메일로 진행해주세요");
+          break;
         case "이름 형식이 올바르지 않습니다.":
           alert("최소 2글자로 입력해주세요");
+          break;
         case "비밀번호 형식이 올바르지 않습니다.":
           alert("8 ~ 20 글자 알파벳 1개 이상 + 숫자 조합");
+          break;
         case "유저가 이미 존재합니다.":
           alert("유저가 이미 존재합니다.");
+          break;
       }
     });
 };
